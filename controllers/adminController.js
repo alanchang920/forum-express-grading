afef9394821ca40b67ae6f519d3d55eb204703ce
Alan Chang <allan850920@hotmail.com>
2021-02-03 17:04:53 +0800

    Add admin restaurant create form

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,7 +6,13 @@ const adminController = {
     return Restaurant.findAll({ raw: true }).then(restaurants => {
       return res.render('admin/restaurants', { restaurants: restaurants })
     })
+  },
+
+  createRestaurant: (req, res) => {
+    return res.render('admin/create')
   }
 }
+
+
 
 module.exports = adminController
