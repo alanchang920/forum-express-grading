573177b61b5fa3ef13414761b0b4c16cb961ce96
Alan Chang <allan850920@hotmail.com>
2021-02-23 18:12:54 +0800

    PUT /api/admin/categories/:id

--- services/categoryService.js
+++ services/categoryService.js
@@ -10,7 +10,7 @@ let categoryService = {
       if (req.params.id) {
         Category.findByPk(req.params.id)
           .then((category) => {
-            return res.render('admin/categories', { categories: categories, category: category })
+            return res.render('admin/categories', { categories: categories, category: category.toJSON() })
           })
       } else {
         callback({ categories: categories })
@@ -26,6 +26,19 @@ let categoryService = {
       })
         .then((category) => {
           callback({ status: 'success', message: 'category was successfully created' })
+        })
+    }
+  },
+  putCategory: (req, res, callback) => {
+    if (!req.body.name) {
+      callback({ status: 'error', message: 'name didn\'t exist' })
+    } else {
+      return Category.findByPk(req.params.id)
+        .then((category) => {
+          category.update(req.body)
+            .then((category) => {
+              callback({ status: 'success', message: 'category was successfully updated' })
+            })
         })
     }
   },
