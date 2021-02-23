4e138043f760c926166670e8af4253f5c1a5ea99
Alan Chang <allan850920@hotmail.com>
2021-02-23 17:22:09 +0800

    POST /api/admin/restaurants

--- services/adminService.js
+++ services/adminService.js
@@ -1,6 +1,10 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const imgur = require('imgur')
+
+imgur.setClientId(process.env.IMGUR_CLIENT_ID);
+imgur.setAPIUrl('https://api.imgur.com/3/');
 
 const adminService = {
   getRestaurants: (req, res, callback) => {
@@ -21,6 +25,45 @@ const adminService = {
     }).then(restaurant => {
       callback({ restaurant: restaurant })
     })
+  },
+
+  postRestaurant: (req, res, callback) => {
+    if (!req.body.name) {
+      return callback({ status: 'error', message: "name didn't exist" });
+    }
+
+    const { file } = req;
+    if (file) {
+      imgur.uploadFile(file.path)
+        .then((img) => {
+          Restaurant.create({
+            name: req.body.name,
+            tel: req.body.tel,
+            address: req.body.address,
+            opening_hours: req.body.opening_hours,
+            description: req.body.description,
+            image: file ? img.data.link : null,
+            CategoryId: req.body.categoryId,
+          }).then((restaurant) => {
+            callback({ status: 'success', message: 'restaurant was successfully created' });
+          });
+        })
+        .catch((err) => {
+          console.error('imgur upload failed', err.message);
+        });
+    } else {
+      return Restaurant.create({
+        name: req.body.name,
+        tel: req.body.tel,
+        address: req.body.address,
+        opening_hours: req.body.opening_hours,
+        description: req.body.description,
+        image: null,
+        CategoryId: req.body.categoryId,
+      }).then((restaurant) => {
+        callback({ status: 'success', message: 'restaurant was successfully created' });
+      });
+    }
   },
 
   deleteRestaurant: (req, res, callback) => {
